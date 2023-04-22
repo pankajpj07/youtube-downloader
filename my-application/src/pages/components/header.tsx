5932f3f41aa4ff1929b24248c5bd4cd59ae5c21c
Pankaj Jain <Pankaj12.pj88@gmail.com>
2023-04-22 19:59:12 +0000

    fixed eslint rules

--- my-application/src/pages/components/header.tsx
+++ my-application/src/pages/components/header.tsx
@@ -1,14 +1,15 @@
 import Link from "next/link"
+import Image from 'next/image'
 import { logoPath } from "../constants/constants"
 
 export default function Header() {
     return (
-        <nav>
+        <div>
             <Link href="/">
             <div className="font-bold py-4 md:py-6 md:px-5 px-2.5 bg-gray-200 text-xl md:text-2xl ">
-                <span className="flex items-center"><img className="w-8" src={logoPath} alt="utubeloader logo" /><span className="text-red-900 ml-3">U</span>TubeLoader</span></div>
+                <span className="flex items-center"><Image width={32} height={32} src={logoPath} alt="utubeloader logo" /><span className="text-red-900 ml-3">U</span>TubeLoader</span></div>
             </Link>
             
-        </nav>
+        </div>
     )
 }
