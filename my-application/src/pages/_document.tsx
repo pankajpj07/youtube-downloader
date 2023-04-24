9ccd07adabe99ad71705c587dc6605dc237220dc
Pankaj Jain <Pankaj12.pj88@gmail.com>
2023-04-24 09:58:29 +0000

    added ga

--- my-application/src/pages/_document.tsx
+++ my-application/src/pages/_document.tsx
@@ -1,9 +1,23 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-MJN5H5RFT4"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-MJN5H5RFT4');
+        `}
+      </Script>
       <body>
         <Main />
         <NextScript />
