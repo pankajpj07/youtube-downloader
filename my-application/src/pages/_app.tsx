366994222249c4d008ced3ffeaf8394adb393406
Pankaj Jain <Pankaj12.pj88@gmail.com>
2023-04-24 00:03:41 +0000

    added google verification

--- my-application/src/pages/_app.tsx
+++ my-application/src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import "tailwindcss/tailwind.css";
-import Head from 'next/head'
-import { AppProps } from 'next/app';
+import Head from "next/head";
+import { AppProps } from "next/app";
 import { logoPath } from "../constants/constants";
 
-function App({ Component, pageProps }:AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
@@ -13,17 +13,25 @@ function App({ Component, pageProps }:AppProps) {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5"
         />
-        <meta name="google-site-verification" content="0k3-dqRQViGsE4wVAWik86L19UwwRo3rWr-b_eQkkfY" />
-        <meta name="description" content="Easily download Youtube videos with our online Youtube downloader. No software installation needed. Save your favorite Youtube videos for free!" />
-        <meta name="keywords" content="Youtube downloader, online video downloader, save Youtube videos, download Youtube videos, Youtube video downloader" />
-        <title>UTubeLoader - Save Youtube Videos Easily</title>
-        <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials"/>
-        <link
-          href={logoPath}
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
+        <meta
+          name="google-site-verification"
+          content="gtnv05yQdxw8nDZ2GPZytDTzmTxGnO29cR5BZ82AwvQ"
         />
+        <meta
+          name="description"
+          content="Easily download Youtube videos with our online Youtube downloader. No software installation needed. Save your favorite Youtube videos for free!"
+        />
+        <meta
+          name="keywords"
+          content="Youtube downloader, online video downloader, save Youtube videos, download Youtube videos, Youtube video downloader"
+        />
+        <title>UTubeLoader - Save Youtube Videos Easily</title>
+        <link
+          rel="manifest"
+          href="/manifest.json"
+          crossOrigin="use-credentials"
+        />
+        <link href={logoPath} rel="icon" type="image/png" sizes="16x16" />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         <meta name="theme-color" content="#317EFB" />
       </Head>
