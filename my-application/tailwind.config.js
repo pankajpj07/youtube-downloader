776306afa55dad0aefbddd319b69b0df26cfe171
Pankaj Jain <Pankaj12.pj88@gmail.com>
2023-04-27 01:53:39 +0000

    fixed css issue

--- my-application/tailwind.config.js
+++ my-application/tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
-    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
