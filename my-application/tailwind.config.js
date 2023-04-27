645fe8a4d314c622b6527b2ca30899418ebe6044
Pankaj Jain <Pankaj12.pj88@gmail.com>
2023-04-27 03:46:42 +0000

    purged the css

--- my-application/tailwind.config.js
+++ my-application/tailwind.config.js
@@ -14,5 +14,10 @@ module.exports = {
       },
     },
   },
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+  ],
   plugins: [],
 }
