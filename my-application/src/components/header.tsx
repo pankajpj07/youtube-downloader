b193780b05e39c4c48194912f5f35616eb85a063
Pankaj Jain <Pankaj12.pj88@gmail.com>
2023-04-27 03:53:45 +0000

    added priority to the image

--- my-application/src/components/header.tsx
+++ my-application/src/components/header.tsx
@@ -1,15 +1,25 @@
-import Link from "next/link"
+import Link from 'next/link'
 import Image from 'next/image'
-import { logoPath } from "../constants/constants"
+import { logoPath } from '../constants/constants'
 
 export default function Header() {
-    return (
-        <div>
-            <Link href="/">
-            <div className="font-bold py-4 md:py-6 md:px-5 px-2.5 bg-gray-200 text-xl md:text-2xl ">
-                <span className="flex items-center"><Image width={32} height={32} src={logoPath} alt="utubeloader logo" title="UtubeLoader Logo"/><span className="text-red-900 ml-3">U</span>TubeLoader</span></div>
-            </Link>
-            
+  return (
+    <div>
+      <Link href="/">
+        <div className="font-bold py-4 md:py-6 md:px-5 px-2.5 bg-gray-200 text-xl md:text-2xl ">
+          <span className="flex items-center">
+            <Image
+              priority
+              width={32}
+              height={32}
+              src={logoPath}
+              alt="utubeloader logo"
+              title="UtubeLoader Logo"
+            />
+            <span className="text-red-900 ml-3">U</span>TubeLoader
+          </span>
         </div>
-    )
+      </Link>
+    </div>
+  )
 }
