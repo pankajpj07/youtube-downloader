b498b98d855fb76e4576bda264eead4282dc0ec3
Pankaj Jain <47389183+pankajpj07@users.noreply.github.com>
2023-04-29 19:31:55 +0530

    Disabled Mp4 feature toggle

--- my-application/config/featureToggle.js
+++ my-application/config/featureToggle.js
@@ -2,7 +2,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 const prodFeatureFlags = {
   isEnableMP3: true,
-  isEnableMP4: true,
+  isEnableMP4: false,
 }
 
 const devFeatureFlags = {
